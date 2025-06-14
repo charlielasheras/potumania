b585b8435cab39fbfc1801ed85cdab1829ac12e0
carlos Lemos <clemos1962@yahoo.com.ar>
2025-06-13 22:21:47 -0300

    actualizacion de fonts

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/* console.log("Hola")
+
+let mensaje = "soy un mensaje en una variable"
+console.log(mensaje)
+
+let numeroA = ""
+confirm.log(numeroA)
+
+//creacion de una constante 
+
+const IVA = 21
+console.log(IVA)
+
+//ingresa valor numerico
+let precio = parseInt("300 pesos")
+console.log(precio)
+
+//para numeros decimales
+let precio = parseFloat ("300 pesos")
+console.log(precio)
+
+//suma de variables
+let numeroA = 3
+let numeroB = 5
+let resultado = numeroA + numeroB
+console.log(resultado)*/
+
+
+
+let numeroA = parseInt(prompt("ingrese un numero: "))
+let numeroB = parseInt(prompt("ingrese un segundo numero: "))
+let resultado = numeroA * numeroB
+console.log(resultado)
